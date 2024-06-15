227a3e05002914395ad32481c8db1305e0bad8a7
daviderazza <daviderazza86@gmail.com>
2024-06-15 13:00:34 +0200

    Preparation for next updates

--- src/main/java/me/redyt008/epicduels/commands/DuelManager.java
+++ src/main/java/me/redyt008/epicduels/commands/DuelManager.java
@@ -2,18 +2,26 @@ package me.redyt008.epicduels.commands;
 
 import me.redyt008.epicduels.EpicDuels;
 import me.redyt008.epicduels.events.preArenaEvent;
+import net.milkbowl.vault.chat.Chat;
 import net.milkbowl.vault.economy.Economy;
 import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
+import org.bukkit.Material;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.configuration.InvalidConfigurationException;
+import org.bukkit.enchantments.Enchantment;
 import org.bukkit.entity.Player;
+import org.bukkit.inventory.Inventory;
+import org.bukkit.inventory.ItemFlag;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.meta.ItemMeta;
 import org.bukkit.plugin.Plugin;
 import org.jetbrains.annotations.NotNull;
 
 import java.io.IOException;
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.UUID;
 
@@ -50,8 +58,31 @@ public class DuelManager implements CommandExecutor {
             if (target != null) {
                 if(!target.getName().equals(player.getName())){
                     if(requests.containsKey(player.getUniqueId())){
-                        player.sendMessage(ChatColor.DARK_RED + "Hai gia nessuna richiesta di duello");
+                        player.sendMessage(ChatColor.DARK_RED + "Hai gia una richiesta di duello");
                     }else {
+
+                        //WORK IN PROGRESS
+                        Inventory inventory = Bukkit.createInventory(null, 27, ChatColor.GOLD + "Selezione Modalità Duello");
+
+                        ItemStack classicDuel = new ItemStack(Material.DIAMOND_SWORD);
+                        ItemMeta classicMeta = classicDuel.getItemMeta();
+                        classicMeta.setDisplayName(ChatColor.GREEN + "Duello Classico");
+                        classicMeta.setLore(Arrays.asList(ChatColor.WHITE + "Duello classico in cui puoi", ChatColor.WHITE + "Alzare o abbassare il tuo Rank"));
+                        classicDuel.setItemMeta(classicMeta);
+
+                        ItemStack friendlyDuel = new ItemStack(Material.RED_TULIP);
+                        ItemMeta friendlyMeta = friendlyDuel.getItemMeta();
+                        friendlyMeta.setDisplayName(ChatColor.GREEN + "Duello Classico");
+                        friendlyMeta.setLore(Arrays.asList(ChatColor.WHITE + "Duello amichevole in cui non si può", ChatColor.WHITE + "Alzare o abbassare il proprio Rank", ChatColor.GOLD + "" + ChatColor.BOLD + "Novità!"));
+                        friendlyMeta.addEnchant(Enchantment.MENDING, 1, false);
+                        friendlyMeta.addItemFlags(ItemFlag.HIDE_ENCHANTS);
+                        friendlyDuel.setItemMeta(friendlyMeta);
+
+                        inventory.setItem(11, classicDuel);
+                        inventory.setItem(15, friendlyDuel);
+                        //WORK IN PROGRESS
+
+
                         requests.put(target.getUniqueId(), player.getUniqueId());
                         player.sendMessage(ChatColor.GOLD + "Hai mandato una richiesta di duello a " + target.getName() + ".");
                         target.sendMessage(ChatColor.GOLD + player.getName() + " ti ha mandato una richiesta di duello");
